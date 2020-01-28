16e6957b548e5a0102e0d3572ef40c0133e095fe
Rahul255 <rahulmohan255@gmail.com>
2020-01-28 15:42:32 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+var destination = document.querySelector("#container")
+
+ReactDOM.render(
+  <div>
+        <p>Welcome Rahul</p>
+    </div>,
+    destination
+);
