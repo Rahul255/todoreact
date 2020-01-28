c93a9c449134568e7c8ea8999b6af93a8d1d07be
Rahul255 <rahulmohan255@gmail.com>
2020-01-28 16:22:39 +0530

    add TodoList.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import TodoList from "./TodoList";
+
 
 var destination = document.querySelector("#container")
 
 ReactDOM.render(
   <div>
-        <p>Welcome Rahul</p>
+        <TodoList/>
     </div>,
     destination
 );
